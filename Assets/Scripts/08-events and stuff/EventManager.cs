921725b96dc088d9895f130e0cba4d607f7ed1da
qwirker <qwirker@ibrumley.com>
2015-10-08 15:16:26 -0400

    some cleanup and working on 08

---
+++ Assets/Scripts/08-events and stuff/EventManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+
+    public delegate void selectionAction();
+    public static event selectionAction selected;
+
+
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity)) {
+            //hitInfo.transform.gameObject.GetComponent<TileScript>().myName = "yo";
+
+            selected();
+
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/08-events and stuff/TileScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+
+    public string myName = "first";
+    public Sprite selectionHex;
+
+
+    void OnEnable() {
+        EventManager.selected += ImSelected;
+    }
+
+    void OnDisable() {
+        EventManager.selected -= ImSelected;
+    }
+
+
+    void ImSelected() {
+        Debug.Log(myName);
+    }
+
+
+
+}
